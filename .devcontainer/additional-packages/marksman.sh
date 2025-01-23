e4923274a1ecd702406a68ccfe44ca93d1470060
Michael Howard <contact@michaelhoward.kiwi>
2025-01-24 12:35:14 +1300

    Add marksman install script

---
+++ .devcontainer/additional-packages/marksman.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Installing Marksman"
+
+set -eux;
+dpkgArch="$(dpkg --print-architecture)";
+case "${dpkgArch##*-}" in
+    amd64) target='linux-x64' ;;
+    *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;;
+esac;
+
+url="https://api.github.com/repos/zellij-org/zellij/releases/latest"
+curl -L -o marksman `curl -s "$url" | jq -r '.assets[].browser_download_url' | grep "$target"`
+chmod +x marksman
+mv marksman /usr/local/bin
